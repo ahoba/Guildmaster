bb1e98350cc8fbd93cf16e71b26bd9cd12828220
Unknown <danken.fk@gmail.com>
2021-02-16 16:02:56 -0300

    TileControlsRefactor - missing files
    Missed some files on last commit

---
+++ Tools/Scenes/Tiles/MapEditorControl.cs
@@ -0,0 +1,114 @@
+﻿using Danke.Scenes.Tiles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tools.Scenes
+{
+    public partial class MapEditorControl : UserControl
+    {
+        private TileSet _tileSet;
+
+        private MapEditMode EditMode { get; set; } = MapEditMode.Add;
+
+        public TileSet TileSet 
+        {
+            get => _tileSet; 
+            set
+            {
+                _tileSet = value;
+
+                if (_tileSet != null)
+                {
+                    tileSetControlTileSet.Tiles = _tileSet.Tiles;
+                }
+            }
+        }
+
+        public Tile[][] Map 
+        {
+            get => tileSetControlMap.Tiles;
+            set
+            { 
+                tileSetControlMap.Tiles = value;
+
+                if (tileSetControlMap.Tiles != null && tileSetControlMap.Tiles[0] != null)
+                {
+                    numericUpDownMapHeight.Value = tileSetControlMap.Tiles.Length;
+
+                    numericUpDownMapWidth.Value = tileSetControlMap.Tiles[0].Length;
+                }
+            }
+        }
+        
+        public MapEditorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void tileSetControlMap_SelectedTileChanged(object sender, EventArgs e)
+        {
+            if (EditMode == MapEditMode.Add)
+            {
+                if (tileSetControlTileSet.SelectedTile != null)
+                {
+                    tileSetControlMap.SelectedTile = tileSetControlTileSet.SelectedTile;
+                }
+            }
+            else if (EditMode == MapEditMode.Erase)
+            {
+                tileSetControlMap.SelectedTile = null;
+            }
+        }
+
+        private void numericUpDownMapWidth_ValueChanged(object sender, EventArgs e)
+        {
+            int newWidth = (int)((NumericUpDown)sender).Value;
+
+            for (int i = 0; i < Map.Length; i++)
+            {
+                Array.Resize(ref Map[i], newWidth);
+            }
+
+            Map = Map; // Force Refresh
+        }
+
+        private void numericUpDownMapHeight_ValueChanged(object sender, EventArgs e)
+        {
+            int newHeight = (int)((NumericUpDown)sender).Value;
+
+            Tile[][] newMap = new Tile[newHeight][];
+
+            for (int i = 0; i < newHeight; i++)
+            {
+                newMap[i] = i >= Map.Length ? 
+                    new Tile[Map[0].Length] :
+                    Map[i];
+            }
+
+            Map = newMap;
+        }
+
+        private enum MapEditMode
+        {
+            Add,
+            Erase
+        }
+
+        private void toolStripButtonEraser_Click(object sender, EventArgs e)
+        {
+            EditMode = MapEditMode.Erase;
+        }
+
+        private void tileSetControlTileSet_SelectedTileChanged(object sender, EventArgs e)
+        {
+            EditMode = MapEditMode.Add;
+        }
+    }
+}
---
+++ Tools/Scenes/Tiles/TileSetControl.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Tools.Scenes
+{
+    partial class TileSetControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            // 
+            // panel
+            // 
+            this.panel.AutoScroll = true;
+            this.panel.Controls.Add(this.pictureBox);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(364, 343);
+            this.panel.TabIndex = 1;
+            // 
+            // TileSetControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel);
+            this.Name = "TileSetControl";
+            this.Size = new System.Drawing.Size(364, 343);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Panel panel;
+    }
+}
