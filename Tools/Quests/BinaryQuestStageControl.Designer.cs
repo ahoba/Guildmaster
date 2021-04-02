52f23652815608cedadfafaf8b437180b9a027dd
Ken <danken.fk@gmail.com>
2021-04-02 16:52:53 -0300

    Binary Quest Stage Control design

---
+++ Tools/Quests/BinaryQuestStageControl.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Tools.Quests
+{
+    partial class BinaryQuestStageControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxSuccessText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonNextStageRoll = new System.Windows.Forms.Button();
+            this.listBoxTests = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxStartingText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFailureText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonAddFailureStage = new System.Windows.Forms.Button();
+            this.buttonAddSuccessStage = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.textBoxFailureText);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBoxSuccessText);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.buttonNextStageRoll);
+            this.panel1.Controls.Add(this.listBoxTests);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBoxStartingText);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(231, 273);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBoxSuccessText
+            // 
+            this.textBoxSuccessText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxSuccessText.Location = new System.Drawing.Point(0, 177);
+            this.textBoxSuccessText.Name = "textBoxSuccessText";
+            this.textBoxSuccessText.Size = new System.Drawing.Size(231, 20);
+            this.textBoxSuccessText.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Success Text";
+            // 
+            // buttonNextStageRoll
+            // 
+            this.buttonNextStageRoll.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNextStageRoll.Location = new System.Drawing.Point(0, 141);
+            this.buttonNextStageRoll.Name = "buttonNextStageRoll";
+            this.buttonNextStageRoll.Size = new System.Drawing.Size(231, 23);
+            this.buttonNextStageRoll.TabIndex = 4;
+            this.buttonNextStageRoll.Text = "Next Stage Roll";
+            this.buttonNextStageRoll.UseVisualStyleBackColor = true;
+            // 
+            // listBoxTests
+            // 
+            this.listBoxTests.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBoxTests.FormattingEnabled = true;
+            this.listBoxTests.Location = new System.Drawing.Point(0, 46);
+            this.listBoxTests.Name = "listBoxTests";
+            this.listBoxTests.Size = new System.Drawing.Size(231, 95);
+            this.listBoxTests.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tests";
+            // 
+            // textBoxStartingText
+            // 
+            this.textBoxStartingText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxStartingText.Location = new System.Drawing.Point(0, 13);
+            this.textBoxStartingText.Name = "textBoxStartingText";
+            this.textBoxStartingText.Size = new System.Drawing.Size(231, 20);
+            this.textBoxStartingText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting Text";
+            // 
+            // textBoxFailureText
+            // 
+            this.textBoxFailureText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxFailureText.Location = new System.Drawing.Point(0, 210);
+            this.textBoxFailureText.Name = "textBoxFailureText";
+            this.textBoxFailureText.Size = new System.Drawing.Size(231, 20);
+            this.textBoxFailureText.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Location = new System.Drawing.Point(0, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Failure Text";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonAddFailureStage, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonAddSuccessStage, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 230);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(231, 43);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // buttonAddFailureStage
+            // 
+            this.buttonAddFailureStage.BackColor = System.Drawing.Color.Tomato;
+            this.buttonAddFailureStage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAddFailureStage.Location = new System.Drawing.Point(3, 3);
+            this.buttonAddFailureStage.Name = "buttonAddFailureStage";
+            this.buttonAddFailureStage.Size = new System.Drawing.Size(109, 37);
+            this.buttonAddFailureStage.TabIndex = 0;
+            this.buttonAddFailureStage.Text = "Add Failure Stage";
+            this.buttonAddFailureStage.UseVisualStyleBackColor = false;
+            // 
+            // buttonAddSuccessStage
+            // 
+            this.buttonAddSuccessStage.BackColor = System.Drawing.Color.OliveDrab;
+            this.buttonAddSuccessStage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAddSuccessStage.Location = new System.Drawing.Point(118, 3);
+            this.buttonAddSuccessStage.Name = "buttonAddSuccessStage";
+            this.buttonAddSuccessStage.Size = new System.Drawing.Size(110, 37);
+            this.buttonAddSuccessStage.TabIndex = 1;
+            this.buttonAddSuccessStage.Text = "Add Success Stage";
+            this.buttonAddSuccessStage.UseVisualStyleBackColor = false;
+            // 
+            // BinaryQuestStageControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "BinaryQuestStageControl";
+            this.Size = new System.Drawing.Size(231, 273);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxStartingText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxTests;
+        private System.Windows.Forms.Button buttonNextStageRoll;
+        private System.Windows.Forms.TextBox textBoxSuccessText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFailureText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonAddFailureStage;
+        private System.Windows.Forms.Button buttonAddSuccessStage;
+    }
+}
