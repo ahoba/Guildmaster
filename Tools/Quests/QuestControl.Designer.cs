32960b8a19800242886dfaf6bf3026ed54d75998
Ken <danken.fk@gmail.com>
2021-04-02 17:25:59 -0300

    Quest Control design

---
+++ Tools/Quests/QuestControl.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Tools.Quests
+{
+    partial class QuestControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBoxDescription = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxSuccessText = new System.Windows.Forms.TextBox();
+            this.textBoxFailureText = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonAddInitialStage = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxTitle, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBoxDescription, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxSuccessText, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxFailureText, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.buttonAddInitialStage, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(820, 593);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxTitle.Location = new System.Drawing.Point(81, 3);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(736, 20);
+            this.textBoxTitle.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Description";
+            // 
+            // richTextBoxDescription
+            // 
+            this.richTextBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxDescription.Location = new System.Drawing.Point(81, 29);
+            this.richTextBoxDescription.Name = "richTextBoxDescription";
+            this.richTextBoxDescription.Size = new System.Drawing.Size(736, 96);
+            this.richTextBoxDescription.TabIndex = 3;
+            this.richTextBoxDescription.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Success Text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Failure Text";
+            // 
+            // textBoxSuccessText
+            // 
+            this.textBoxSuccessText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxSuccessText.Location = new System.Drawing.Point(81, 131);
+            this.textBoxSuccessText.Name = "textBoxSuccessText";
+            this.textBoxSuccessText.Size = new System.Drawing.Size(736, 20);
+            this.textBoxSuccessText.TabIndex = 6;
+            // 
+            // textBoxFailureText
+            // 
+            this.textBoxFailureText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxFailureText.Location = new System.Drawing.Point(81, 157);
+            this.textBoxFailureText.Name = "textBoxFailureText";
+            this.textBoxFailureText.Size = new System.Drawing.Size(736, 20);
+            this.textBoxFailureText.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 212);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(814, 378);
+            this.panel1.TabIndex = 8;
+            // 
+            // buttonAddInitialStage
+            // 
+            this.buttonAddInitialStage.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonAddInitialStage.Location = new System.Drawing.Point(81, 183);
+            this.buttonAddInitialStage.Name = "buttonAddInitialStage";
+            this.buttonAddInitialStage.Size = new System.Drawing.Size(115, 23);
+            this.buttonAddInitialStage.TabIndex = 9;
+            this.buttonAddInitialStage.Text = "Add Initial Stage";
+            this.buttonAddInitialStage.UseVisualStyleBackColor = true;
+            // 
+            // QuestControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "QuestControl";
+            this.Size = new System.Drawing.Size(820, 593);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBoxDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxSuccessText;
+        private System.Windows.Forms.TextBox textBoxFailureText;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonAddInitialStage;
+    }
+}
